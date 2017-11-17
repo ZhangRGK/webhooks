9eabc814b6786aa1a6b9279ac401864b062dfc30
ZhangRGK <stamen0630@gmail.com>
2017-11-17 14:30:43 +0800

    add web server

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+const config = require('./config');
+const handler = createHandler(config);
+
+http.createServer((req, res) => {
+  handler(req, res, (error) => {
+    res.statusCode = 404;
+    res.end('no such location');
+  })
+}).listen(8001);
+
+handler.once('error', console.log);
+
+handler.on('push', ({ ref }) => {
+  // TODO run test
+});
+
+handler.on('push_request', ({ action, pull_request }) => {
+  const branch = pull_request.base.ref;
+  console.log(action, branch);
+
+  if (action === 'closed'
+    && branch.test(/feature\/[\d+\.*]+/i)
+    && pull_request.base.merged
+  ) {
+    // TODO run build scripts
+  }
+});
+
+handler.once('release', ({ action, release }) => {
+  // TODO build
+});
